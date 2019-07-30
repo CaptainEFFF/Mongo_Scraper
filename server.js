fa613dd9d819994d9a635d51776a4b3a4c1153de
CaptainEFFF <davidbanville4@gmail.com>
2019-07-30 18:29:09 -0400

    scraping reddit

--- server.js
+++ server.js
@@ -32,25 +32,20 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // Routes
 
-// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://old.reddit.com/r/ProgrammerHumor/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    
+    $("p.title").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $(this).text();
+      result.link = $(this).children().attr("href");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
