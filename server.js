79db02b9351ef30972223ca891c3c0eea8d5ce04
CaptainEFFF <davidbanville4@gmail.com>
2019-07-30 18:53:34 -0400

    port fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
